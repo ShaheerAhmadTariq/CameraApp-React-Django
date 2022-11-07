2b6ce86d59339a917229bafde9dd5ceb1b84bf56
unknown <shaheerahmadtariq@gmail.com>
2022-11-07 22:42:38 +0500

    <Face Detection with frames showing on react>

--- api/views.py
+++ api/views.py
@@ -10,13 +10,16 @@ from .models import Task
 # Create your views here.
 import io
 import base64
-import json
+import numpy as np
+import cv2
 from django.core.files.base import ContentFile
 from rest_framework.renderers import JSONRenderer
 from rest_framework.request import Request
 from rest_framework.test import APIRequestFactory
 from rest_framework.parsers import JSONParser
 
+
+listOfImages = []
 
 def video(data):
 	fh = open("./video.mp4", "wb")
@@ -25,11 +28,42 @@ def video(data):
 	print("\n\nFile has been created\n\n")
 
 
-def image(data):
-	fh = open("./image.jpeg", "wb")
-	fh.write(base64.b64decode(data))
-	fh.close()
-	print("\n\nFile has been created\n\n")
+def image(img):
+	decoded_data = base64.b64decode(img)
+	np_data = np.frombuffer(decoded_data,np.uint8)
+	img = cv2.imdecode(np_data,cv2.IMREAD_UNCHANGED)
+	# img = cv2.imread('./img.jpg')
+	face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	# Detect faces
+	faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+	# Draw rectangle around the faces
+	for (x, y, w, h) in faces:
+		cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+	# im_bytes = img.tobytes()
+	# im_b64 = base64.b64encode(im_bytes)
+	# parsed = im_b64.decode('utf-8')
+	img_encode = cv2.imencode('.jpeg', img)[1]
+	img_encode.tobytes()
+	baseImg=base64.b64encode(img_encode)
+	img = baseImg.decode('utf-8')
+	return img
+	# fh = open("./image.jpeg", "wb")
+	# fh.write(base64.b64decode(data))
+	# fh.close()
+	# print("\n\nFile has been created\n\n")
+def countImages(data):
+	listOfImages.append(data)
+	print(len(listOfImages))
+@api_view(['GET'])
+def createVideo(request):
+	frameSize = (640, 360)
+	out = cv2.VideoWriter('output_video.avi',cv2.VideoWriter_fourcc(*'DIVX'), 60, frameSize)
+	for val in listOfImages:
+		img = cv2.imread(val)
+		out.write
+	out.release()
+	return Response('Video Created')
 @api_view(['GET'])
 def apiOverview(request):
 	api_urls = {
@@ -57,14 +91,25 @@ def taskDetail(request, pk):
 
 @api_view(['POST'])
 def taskCreate(request):
-	# parsed = request.body.decode("utf-8") 
-	# # print(parsed)
+	
+	parsed = request.body.decode("utf-8") 
+	# print('parsed: ',parsed)
+	firstHalf = parsed.split('base64,')
+	secondHalf = firstHalf[1].split('------WebKit')
+	data= secondHalf[0].strip()
 	# dataLen = len(parsed) - 44
 	# data = parsed[113:dataLen]
-	# # print("len of string",len(data),data)
-	# # print(data)
-	# image(data)
-	return Response('got your image')
+	# print("len of string",len(data),data)
+	img = image(data)
+	data = 'data:image/jpeg;base64,' + img
+	
+	# print(parsed)
+	# dataLen = len(parsed) - 44
+	# data = parsed[113:dataLen]
+	# print("len of string",len(data),data)
+	# print(data)
+	
+	return Response(data)
 @api_view(['POST'])
 def taskUpdate(request, pk):
 	task = Task.objects.get(id=pk)
--- frontend/src/components/TestImage.js
+++ frontend/src/components/TestImage.js
@@ -4,22 +4,25 @@ function TestImage() {
   // const localVideo = React.useRef();
   let t0
   let t1
-  
+const [imgState,setImageState]=React.useState("")
 function getVideo()
 {
+  
   
   navigator.getUserMedia = navigator.getUserMedia ||
                         navigator.webkitGetUserMedia ||
                         navigator.mozGetUserMedia;
 
       if (navigator.getUserMedia) {
-            navigator.getUserMedia({ audio: true, video: { width: 1280, height: 720 } },
+            navigator.getUserMedia({ audio: false, video: { width: 1280, height: 720 } },
                 (stream) => {
-                  
+                  console.log("starting video")
                   const video = document.getElementById('localVideo');
                   var canvas = document.querySelector('canvas');
                   var ctx = canvas.getContext('2d');
                   video.srcObject = stream;
+                  const image1 = document.getElementById('localPhoto')
+                  
                   video.onloadedmetadata = (e) => {
                     video.play();
                   };
@@ -39,14 +42,15 @@ function getVideo()
                             }
                             data.append('video',image_base64);
                             axios.post(url, data, config).then(response => {
-                                  // console.log(response.data)
+                                  console.log(response.data)
+                                setImageState(response.data)
                                   t1= performance.now(); //end time
                                   // console.log('Time taken to capture and send one frame from react and get response from Django is:'+ (t1-t0) +' milliseconds');
                                 })
                                   video.requestVideoFrameCallback(updateCanvas); 
                                   
                           }
-                          const myTimeout = setTimeout(stopVideo, 1000);
+                          
                           video.requestVideoFrameCallback(updateCanvas);  
                 },
                 (err) => {
@@ -64,13 +68,31 @@ function getVideo()
     video.pause();
     console.log("Stopped")
   }
-  
+  function stopButton()
+  {
+    console.log("Stop button clicked")
+    const myTimeout = setTimeout(stopVideo, 1000);
+
+  }
+  function createVideo()
+  {
+    var url = 'http://127.0.0.1:8000/api/createVideo/'
+                          
+                            
+                            axios.post(url, 'data').then(response => {
+                                  console.log(response.data)
+                                 
+                                })
+  }
   return (
     <>
-    <video id="localVideo" ></video>
+    <video width="640" height="360"id="localVideo" ></video>
     <button id="startButton" onClick={getVideo}>Start</button>
-    <button id="startButton" onClick={stopVideo}>Stop</button>
+    <button id="stopButton" onClick={stopButton}>Stop</button>
+    <button onClick={createVideo}>CreateVideo</button>
     <canvas width="640" height="360"></canvas>
+    <img id='localPhoto' src={imgState} alt="backend-image" />
+    {/* <video id='backendImage'></video> */}
     </>
   )
 }
